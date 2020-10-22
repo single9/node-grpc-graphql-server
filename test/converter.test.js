7b24897929d0ad978909cd368a43052072d6fc66
Duye Chen <k078264@gmail.com>
2020-10-22 16:06:35 +0800

    Update test case

--- test/converter.test.js
+++ test/converter.test.js
@@ -1,13 +1,19 @@
-const should = require('should');
+require('should');
 const grpc = require('grpc');
 const protoLoader = require('@grpc/proto-loader');
 const { gql } = require('apollo-server-express');
 const converter = require('../converter');
+const { readProtofiles } = require('../tools');
 
 describe('Test converter', () => {
-  it('should convert the protobuf object to GraphQL schema', (done) => {
-    const packageDefinition = protoLoader.loadSync(__dirname + '/../examples/protos/hello.proto');
-    const packageDefinitionObjects = grpc.loadPackageDefinition(packageDefinition);
+  let packageDefinition, packageDefinitionObjects;
+
+  before(() => {
+    packageDefinition = protoLoader.loadSync(readProtofiles(__dirname + '/../examples/protos'));
+    packageDefinitionObjects = grpc.loadPackageDefinition(packageDefinition);
+  });
+
+  it('should convert the helloworld protobuf object to GraphQL schema', (done) => {
     const gqlSchema = converter(packageDefinitionObjects, [
       {
         name: 'helloworld',
@@ -18,10 +24,42 @@ describe('Test converter', () => {
         ]
       }
     ]);
+    gqlSchema.should.be.String();
+
     const gqlDefinition = gql`${gqlSchema}`;
     gqlDefinition.definitions.find(def => def.name.value === 'helloworld').should.be.Object();
     gqlDefinition.definitions.find(def => def.name.value === 'Greeter').should.be.Object();
+    (gqlDefinition.definitions.find(def => def.name.value === 'calculator') === undefined).should.be.True();
     gqlSchema.should.be.String();
+    done();
+  });
+
+  it('should convert the multiple protobuf object to GraphQL schema', (done) => {
+    const gqlSchema = converter(packageDefinitionObjects, [
+      {
+        name: 'helloworld',
+        services: [
+          {
+            name: 'Greeter',
+          }
+        ]
+      },
+      {
+        name: 'calculator',
+        services: [
+          {
+            name: 'Calculator',
+          }
+        ]
+      }
+    ]);
+    gqlSchema.should.be.String();
+
+    const gqlDefinition = gql`${gqlSchema}`;
+    gqlDefinition.definitions.find(def => def.name.value === 'helloworld').should.be.Object();
+    gqlDefinition.definitions.find(def => def.name.value === 'Greeter').should.be.Object();
+    gqlDefinition.definitions.find(def => def.name.value === 'calculator').should.be.Object();
+    gqlDefinition.definitions.find(def => def.name.value === 'Calculator').should.be.Object();
     done();
   });
 });
