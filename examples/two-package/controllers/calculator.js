cf73801ec8b113143a01a3cd6af6bb4f0e583ecf
Duye Chen <k078264@gmail.com>
2020-11-02 14:56:19 +0800

    Change types and add some examples

---
+++ examples/two-package/controllers/calculator.js
@@ -0,0 +1,41 @@
+
+const { Controller } = require('../../../index.js');
+
+class Calculator extends Controller {
+  add(call, callback) {
+    let res = {
+      result: call.request.a + call.request.b
+    }
+    return this.response(res, callback);
+  }
+
+  minus(call, callback) {
+    let res = {
+      result: call.request.a - call.request.b
+    }
+    return this.response(res, callback);
+  }
+
+  sqrt(call, callback) {
+    let res = {
+      result: Math.sqrt(call.request.x)
+    };
+    return this.response(res, callback);
+  }
+
+  multiply(call, callback) {
+    let res = {
+      result: call.request.a * call.request.b
+    };
+    return this.response(res, callback);
+  }
+
+  devide(call, callback) {
+    let res = {
+      result: call.request.a / call.request.b
+    };
+    return this.response(res, callback);
+  }
+}
+
+module.exports = Calculator;
---
+++ examples/two-package/grpc-client.js
@@ -0,0 +1,47 @@
+const { RPCClient } = require('../../index.js');
+const rpcClient = new RPCClient({
+  protoFile: __dirname + '/../protos/',
+  packages: [
+    {
+      name: 'helloworld',
+      services: [
+        {
+          name: 'Greeter',
+        }
+      ]
+    },
+    {
+      name: 'calculator',
+      services: [
+        {
+          name: 'Simple',
+        },
+        {
+          name: 'Complex',
+        }
+      ]
+    },
+  ]
+});
+
+async function main() {
+  // call with callback
+  rpcClient.helloworld.Greeter.SayHelloAgain({ name: 'test again' }, function (err, response) {
+    if (err) return console.log('no response');
+    console.log('Greeting again:', response.message);
+  });
+
+  // call with promise
+  const sayHelloResponse = await rpcClient.helloworld.Greeter.SayHello({ name: 'test' });
+  const SayNestedResponse = await rpcClient.helloworld.Greeter.SayNested({});
+  console.log('Greeting', sayHelloResponse.message);
+  console.log(SayNestedResponse);
+
+  const added = await rpcClient.calculator.Simple.add({a: 1, b: 5});
+  console.log('add:',added);
+
+  const sqrt = await rpcClient.calculator.Complex.sqrt({x: 2});
+  console.log('sqrt:', sqrt);
+}
+
+main();
