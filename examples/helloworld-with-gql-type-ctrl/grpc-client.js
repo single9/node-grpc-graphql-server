daf79e7cddc59b08aeb42093698cba37e7bbb8fd
Duye Chen <k078264@gmail.com>
2021-09-13 10:58:18 +0800

    chore: add examples

---
+++ examples/helloworld-with-gql-type-ctrl/grpc-client.js
@@ -0,0 +1,31 @@
+const { RPCClient } = require('../../index.js');
+
+const rpcClient = new RPCClient({
+  protoFile: `${__dirname}/../protos/hello.proto`,
+  packages: [
+    {
+      name: 'helloworld',
+      services: [
+        {
+          name: 'Greeter',
+        },
+      ],
+    },
+  ],
+});
+
+async function main() {
+  // call with callback
+  rpcClient.helloworld.Greeter.SayHelloAgain({ name: 'test again' }, (err, response) => {
+    if (err) return console.log('no response');
+    console.log('Greeting again:', response.message);
+  });
+
+  // call with promise
+  const sayHelloResponse = await rpcClient.helloworld.Greeter.SayHello({ name: 'test' });
+  const SayNestedResponse = await rpcClient.helloworld.Greeter.SayNested({});
+  console.log('Greeting', sayHelloResponse.message);
+  console.log(SayNestedResponse);
+}
+
+main();
---
+++ examples/helloworld-with-gql-type-ctrl/index.js
@@ -0,0 +1,47 @@
+const app = require('express')();
+const { RPCServer } = require('../../index.js');
+const Hello = require('./controllers/helloworld.js');
+
+const methods = {
+  hello: new Hello(),
+};
+
+const rpcServer = new RPCServer({
+  // port: 50052,    // uncomment to set gRPC port on 50052
+  graphql: true,
+  grpc: {
+    protoFile: `${__dirname}/../protos/hello.proto`,
+    packages: [
+      {
+        name: 'helloworld',
+        services: [
+          {
+            name: 'Greeter',
+            implementation: methods.hello,
+            mutate: [
+              'SayHello',
+              'SayHelloWithGender',
+            ],
+            query: [
+              'SayIntArray',
+              'SayNested',
+            ]
+          },
+        ],
+      },
+    ],
+  }
+});
+
+rpcServer.once("grpc_server_started", async (payload) => {
+  console.log("gRPC server started on %s:%d", payload.ip, payload.port);
+});
+
+if (rpcServer.gqlServer) {
+  rpcServer.gqlServer.applyMiddleware({ app });
+}
+
+app.listen(3000, () => {
+  console.log('Server started. http://localhost:3000');
+  console.log('  GraphQL http://localhost:3000/graphql');
+});
