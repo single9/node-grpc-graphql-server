432044a84a8ae35a81061722f8d9a0f4a032eb1e
Duye Chen <k078264@gmail.com>
2022-07-19 10:42:48 +0800

    Revert "chore: update tools.test.ts"
    This reverts commit a0b9c4b260b11297dbdd4fc30ecf25c2869c405d.

--- test/libs/tools.test.ts
+++ test/libs/tools.test.ts
@@ -162,6 +162,6 @@ describe('Test libs/tools', () => {
   });
 
   afterAll(() => {
-    fs.rmSync(`${__dirname}/tmp`, { recursive: true });
+    fs.rmdirSync(`${__dirname}/tmp`, { recursive: true });
   });
 });
